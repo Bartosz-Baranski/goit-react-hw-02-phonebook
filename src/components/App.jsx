b3208137be1b94fff2f43437d9d625032a03e7df
BartoszBaranski90 <bartosz.baranski1990@gmail.com>
2023-10-21 10:45:10 +0200

    Creat reppo
    create repo, instal node, add folder Phonebookand file jsx or css.module

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import Phonebook from './Feedback/Phonebook';
+
+export function App() {
+  return (
+    <>
+      {/* <Phonebook /> */}
+    </>
+  );
+}
---
+++ src/components/Feedback/Phonebook.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import css from './Phonebook.module.css';
+
+class Phonebook extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+}
+export default Phonebook;
